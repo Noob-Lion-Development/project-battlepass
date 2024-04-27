154661a564dd6d6058b162b4a744ba642b4ca7e3
Noel Abellanosa <abellnoel99@gmail.com>
2024-04-27 14:04:31 -0400

    style: add code formatting tools and format project

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,28 +1,36 @@
 <template>
-  <ion-page>
-    <ion-tabs>
-      <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
-        </ion-tab-button>
+	<ion-page>
+		<ion-tabs>
+			<ion-router-outlet></ion-router-outlet>
+			<ion-tab-bar slot="bottom">
+				<ion-tab-button href="/tabs/tab1" tab="tab1">
+					<ion-icon :icon="triangle" aria-hidden="true" />
+					<ion-label>Tab 1</ion-label>
+				</ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
+				<ion-tab-button href="/tabs/tab2" tab="tab2">
+					<ion-icon :icon="ellipse" aria-hidden="true" />
+					<ion-label>Tab 2</ion-label>
+				</ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
-    </ion-tabs>
-  </ion-page>
+				<ion-tab-button href="/tabs/tab3" tab="tab3">
+					<ion-icon :icon="square" aria-hidden="true" />
+					<ion-label>Tab 3</ion-label>
+				</ion-tab-button>
+			</ion-tab-bar>
+		</ion-tabs>
+	</ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+<script lang="ts" setup>
+import {
+	IonIcon,
+	IonLabel,
+	IonPage,
+	IonRouterOutlet,
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
+} from "@ionic/vue";
+import { ellipse, square, triangle } from "ionicons/icons";
 </script>
